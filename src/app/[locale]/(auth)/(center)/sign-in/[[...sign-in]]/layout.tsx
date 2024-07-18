d6829dc6370864ea4d83638234edf7f4e04dd79f
hoainambeco <namxg1@gmail.com>
2024-07-18 15:50:05 +0700

    feat: improve social login feature

--- src/app/[locale]/(auth)/(center)/sign-in/[[...sign-in]]/layout.tsx
+++ src/app/[locale]/(auth)/(center)/sign-in/[[...sign-in]]/layout.tsx
@@ -5,7 +5,8 @@ import { signIn, useSession } from 'next-auth/react';
 import useAuthStore from '@/store/auth';
 import { useEffect } from 'react';
 import Image from 'next/image';
-
+import { useTranslations } from 'next-intl';
+localStorage.setItem('theme', 'theme-light');
 const LoginPage: NextPage = () => {
   const login = useAuthStore((state: any) => state.login);
   const { data: session } = useSession();
@@ -14,6 +15,7 @@ const LoginPage: NextPage = () => {
       login(null, session);
     }
   }, [session]);
+  const t = useTranslations('SignIn');
 
   return (
     <div className="main-wrap">
@@ -40,15 +42,15 @@ const LoginPage: NextPage = () => {
         <div
           className="col-xl-5 d-none d-xl-block vh-100 bg-image-cover bg-no-repeat p-0"
           style={{
-            backgroundImage: `url("assets/images/login-bg.jpg")`,
+            backgroundImage: `url("../assets/images/login-bg.jpg")`,
           }}
         />
         <div className="col-xl-7 vh-100 align-items-center d-flex rounded-3 overflow-hidden bg-white">
           <div className="card login-card me-auto ms-auto border-0 shadow-none">
             <div className="card-body rounded-0 text-left">
               <h2 className="fw-700 display1-size display2-md-size mb-3">
-                Sign in with <br />
-                your social account
+                {t('sign_in_with')} <br />
+                {t('your_social_account')}
               </h2>
               <div className="col-sm-12 mt-2 p-0 text-center">
                 <div className="form-group mb-1">
@@ -60,13 +62,13 @@ const LoginPage: NextPage = () => {
                     onClick={() => signIn('google')}
                   >
                     <Image
-                      width={20}
-                      height={20}
+                      width={40}
+                      height={40}
                       src="/assets/images/icon-1.png"
                       alt="icon"
                       className="w40 mb-1 me-5 ms-2"
                     />{' '}
-                    Sign in with Google
+                    {t('sign_in_with_google')}
                   </button>
                 </div>
                 <div className="form-group mb-1">
@@ -77,12 +79,14 @@ const LoginPage: NextPage = () => {
                     className="form-control style2-input fw-600 bg-twiiter border-0 p-0 text-left text-white "
                     onClick={() => signIn('facebook')}
                   >
-                    <img
-                      src="assets/images/icon-3.png"
+                    <Image
+                      width={40}
+                      height={40}
+                      src="/assets/images/icon-3.png"
                       alt="icon"
                       className="w40 mb-1 me-5 ms-2"
                     />{' '}
-                    Sign in with Facebook
+                    {t('sign_in_with_facebook')}
                   </button>
                 </div>
               </div>
