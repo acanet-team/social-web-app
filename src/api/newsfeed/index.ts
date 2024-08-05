fd4eb2247b774e7311e1b2797361cbc22200ee7c
hoainambeco <namxg1@gmail.com>
2024-08-06 00:42:13 +0700

    feat: Update API requests to include headers

--- src/api/newsfeed/index.ts
+++ src/api/newsfeed/index.ts
@@ -1,57 +1,66 @@
-import type { promises } from "dns";
 import httpClient from "../index";
-import { type BaseApiResponse, type PostRequestParams } from "../model";
+import { type PostRequestParams } from "../model";
 import { likeParams, type Comment, type CommentResponse } from "./model";
 import type { T } from "vitest/dist/reporters-yx5ZTtEV.js";
 
 export const header = new Headers();
 
-const token =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Nywicm9sZSI6eyJpZCI6MiwibmFtZSI6InVzZXIiLCJfX2VudGl0eSI6IlJvbGVFbnRpdHkifSwic2Vzc2lvbklkIjo5LCJpYXQiOjE3MjEwMTc3NzcsImV4cCI6MTgwNzQxNzc3N30.gM1-jdute6OTZE6O_ihrMegtlwODZyfu_Nk4GFAR7-Q";
-export const likeRequest = (values: any) => {
-  header.set("Authorization", "Bearer " + token);
+export const likeRequest = (
+  values: any,
+  headers: Headers = undefined as unknown as Headers
+) => {
   return httpClient.post<PostRequestParams<likeParams>, T>("/v1/post", values, {
-    headers: header,
+    headers
   });
 };
 
-export const getComments = (page: number, take: number, postId: string) => {
-  header.set("Authorization", "Bearer " + token);
+export const getComments = (
+  page: number,
+  take: number,
+  postId: string,
+  headers: Headers = undefined as unknown as Headers
+) => {
   return httpClient.get<CommentResponse<Comment>>(
     `/v1/comment?order=DESC&page=${page}&take=${take}&postId=${postId}`,
     {
-      headers: header,
-    },
+      headers,
+    }
   );
 };
 
-export const getPosts = (page: number, take: number, type: string) => {
-  header.set("Authorization", "Bearer " + token);
+export const getPosts = (page: number, take: number, type: string,
+    headers: Headers = undefined as unknown as Headers) => {
   return httpClient.get(
     `/v1/post?page=${page}&take=${take}&newsFeedType=${type}`,
     {
-      headers: header,
+      headers
     },
   );
 };
 
-export const postComment = (values: { content: string; postId: string }) => {
-  header.set("Authorization", "Bearer " + token);
+export const postComment = (
+  values: { content: string; postId: string },
+  headers: Headers = undefined as unknown as Headers
+) => {
   return httpClient.post("/v1/comment", values, {
-    headers: header,
+    headers,
   });
 };
 
-export const deleteComment = (commentId: string) => {
-  header.set("Authorization", "Bearer " + token);
+export const deleteComment = (
+  commentId: string,
+  headers: Headers = undefined as unknown as Headers
+) => {
   return httpClient.delete(`/v1/comment/${commentId}`, {
-    headers: header,
+    headers,
   });
 };
 
-export const deletePost = (postId: string) => {
-  header.set("Authorization", "Bearer " + token);
+export const deletePost = (
+  postId: string,
+  headers: Headers = undefined as unknown as Headers
+) => {
   return httpClient.delete(`v1/post/${postId}`, {
-    headers: header,
+    headers,
   });
 };
