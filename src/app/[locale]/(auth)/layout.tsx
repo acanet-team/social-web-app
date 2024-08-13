313a3c65b4e2a2b9e57a2451446ae52de9a4995a
duyht56 <duyht56@gmail.com>
2024-08-13 17:21:48 +0700

    fix: clean app dir

--- src/app/[locale]/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function CenteredLayout(props: { children: React.ReactNode }) {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      {props.children}
-    </div>
-  );
-}
