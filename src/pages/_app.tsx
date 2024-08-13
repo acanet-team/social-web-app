fb57d983c14932b4cc8225faf8d9c537780b2df3
duyht56 <duyht56@gmail.com>
2024-08-13 17:51:54 +0700

    fix: login

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
 import httpClient from "@/api";
 import { ONBOARDING_STEP, type ISession } from "@/api/auth/auth.model";
-import { AppProvider, useApp } from "@/context/app.context";
+import { AppProvider } from "@/context/app.context";
 import Loading from "@/context/Loading";
 import { LoadingProvider } from "@/context/Loading/context";
-import { useAccessTokenStore } from "@/store/accessToken";
 import "@/styles/global.scss";
 import type { NextPage } from "next";
 import { getSession, SessionProvider } from "next-auth/react";
 import { NextIntlClientProvider } from "next-intl";
 import type { AppProps } from "next/app";
 import App from "next/app";
-import { redirect, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
+import Router from "next/router";
 import type { ReactElement, ReactNode } from "react";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
@@ -23,6 +23,7 @@ type AppPropsWithLayout = AppProps & {
 
 export default function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
+  const router = useRouter();
 
   return (
     <AppProvider session={pageProps.session}>
@@ -57,7 +58,6 @@ MyApp.getInitialProps = async (appContext: any) => {
     appProps.pageProps["session"] = session;
     httpClient.setAuthorization(session?.accessToken);
     if (session.user.onboarding_data?.step !== ONBOARDING_STEP.COMPLETE) {
-      console.log("appContext.ctx.asPath", appContext.ctx.asPath);
       if (
         appContext.ctx.res &&
         appContext.ctx.asPath !== "/onboarding" &&
@@ -67,7 +67,13 @@ MyApp.getInitialProps = async (appContext: any) => {
           Location: "/onboarding",
         });
         appContext.ctx.res.end();
-        return;
+        // return;
+      } else if (
+        !appContext.ctx.res &&
+        appContext.ctx.asPath !== "/onboarding" &&
+        !appContext.ctx.asPath.includes("next")
+      ) {
+        Router.push("/onboarding");
       }
     }
   }
