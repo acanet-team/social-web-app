52a6fbe33f49fedc212807e0330c2c2c2c895202
ly_nguyen <nguyenthihuongly21@gmail.com>
2024-10-04 11:23:19 +0700

    feat: nft tab UI

--- src/components/profile/TabNftProfile.tsx
+++ src/components/profile/TabNftProfile.tsx
@@ -7,6 +7,7 @@ import { useWeb3 } from "@/context/wallet.context";
 import httpClient from "@/api";
 import axios from "axios";
 import { Agent } from "http";
+import styles from "@/styles/modules/TabNftProfile.module.scss";
 
 const TabNftProfile = (props: { user: User; idParam: string }) => {
   const { user, idParam } = props;
@@ -58,56 +59,60 @@ const TabNftProfile = (props: { user: User; idParam: string }) => {
 
   return (
     <>
-      {isLoading && <DotWaveLoader />}
-      {!isLoading && (
-        <div className="row">
-          <div className="col-xl-12 col-12">
-            <div className="card p-4 border-0 shadow-xss rounded-3 overflow-hidden mb-3">
-              <div className="card-body d-block w-100 shadow-xss rounded-3 overflow-hidden p-0">
-                <div className="p-4 d-flex rounded-3 bg-greylight">
-                  <h4 className="fw-700 font-xssss text-grey-900 mb-0">
-                    NFT Profile
-                  </h4>
+      <div className="row">
+        <div className="col-xl-12 col-12">
+          <div className="card p-0 border-0 shadow-xss rounded-3 overflow-hidden my-5">
+            <div className="card-body d-block w-100 overflow-hidden p-0">
+              <div className="ps-0 d-flex">
+                <h4 className="fw-700 font-xss text-grey-900 mb-0 p-4">
+                  NFT Profile
+                </h4>
+              </div>
+              {isLoading && <DotWaveLoader />}
+              {!isLoading && nftData?.length === 0 && (
+                <div className="text-grey-600 mt-3 mb-5 text-center">
+                  No NFTs found
                 </div>
-                <div className="card-body d-block w-100 shadow-xss rounded-3 overflow-hidden p-4 bg-greylight">
-                  {nftData.length > 0 ? (
-                    <div className="row">
-                      {nftData.map((nft, index) => (
-                        <div className="col-lg-4 col-md-4 col-6" key={index}>
-                          <div className="card p-0 border-0 mb-3">
-                            <div className="card-body p-3">
-                              <div className="card-image col-12 p-0 mb-3">
-                                <Image
-                                  src={nft.image_url}
-                                  alt={nft.name ?? "NFT"}
-                                  width={200}
-                                  height={200}
-                                />
-                              </div>
-                              <h5 className="fw-700 font-xssss text-grey-900 mb-2">
-                                {nft.name ?? "NFT"}
-                              </h5>
-                              <p className="fw-500 font-xsssss text-grey-500">
-                                {nft.description.length > 50
-                                  ? `${nft.description.substring(0, 50)}...`
-                                  : nft.description}
-                              </p>
+              )}
+              {!isLoading && nftData?.length > 0 && (
+                <div className="card-body d-block w-100 shadow-xss rounded-3 overflow-hidden p-4">
+                  <div className={`${styles["nft-card_container"]}`}>
+                    {nftData.map((nft, index) => (
+                      <div
+                        className={`${styles["nft-card"]} col-lg-4 col-md-4 col-6`}
+                        key={nftData[0].image_url}
+                      >
+                        <div className="card p-0 border-0 h-100">
+                          <div className="card-body p-3">
+                            <div className="card-image col-12 p-0">
+                              <Image
+                                src={nftData[0].image_url}
+                                alt={nftData[0].name ?? "NFT"}
+                                width={200}
+                                height={200}
+                                className={styles["ntf-image"]}
+                                loading="lazy"
+                              />
                             </div>
+                            <h5 className="fw-700 font-xss text-grey-900 mb-2 mt-3">
+                              {nftData[0].name ?? "NFT"}
+                            </h5>
+                            <p className="fw-500 font-xsss text-grey-500 m-0">
+                              {nftData[0].description.length > 50
+                                ? `${nftData[0].description.substring(0, 80)}...`
+                                : nftData[0].description}
+                            </p>
                           </div>
                         </div>
-                      ))}
-                    </div>
-                  ) : (
-                    <div className="text-grey-600 mt-3 text-center">
-                      No NFTs found
-                    </div>
-                  )}
+                      </div>
+                    ))}
+                  </div>
                 </div>
-              </div>
+              )}
             </div>
           </div>
         </div>
-      )}
+      </div>
     </>
   );
 };
